5ffe165290315047110edff530d330b85397189f
jramirez <ramirez.jocelyn73@yahoo.com>
2021-08-01 12:24:07 -0700

    commit tree f1dde4a7a2f0f9a223e94608c097f02cd6e5e223

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+const img = document.getElementById(`dog-image-container`)
+document.addEventListener(`DOMContentLoaded`,function(){
+    fetchImages();
+})
+
+function fetchImages(){
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(result =>addImages(result))
+}
+
+function addImages(result){
+  console.log(result)
+  //have an array and want to append to the dom
+  // want to display each on. so don't need map.
+  // for each --> img.innerHTML =
+
+  for (const imgs of result){
+      img.innerHTML += imgs 
+  }
+}
